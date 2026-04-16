4e89a46da08435d59db36d1f2eda739948b0cfa9
olayinkakhalid <adewoyekhalid237@gmail.com>
2026-04-16 10:43:15 +0100

    your message here

--- src/Component/PractisePage/Practice.jsx
+++ src/Component/PractisePage/Practice.jsx
@@ -23,7 +23,7 @@ const Practice = () => {
           </p>
 
           <p className="text-sm md:text-lg max-w-xl mx-auto">
-            Dive deeper by learning HTML, simple and easily understood. <br />
+            Dive deeper by Practising simple and easily understand codes. <br />
             <span className="text-green-500">Code with Another World</span>
           </p>
 
@@ -72,6 +72,54 @@ const Practice = () => {
 
         </div>
 
+      </div>
+
+
+      <div className="text-center space-y-2 md:space-y-3 py-4 md:py-6">
+        <h1 className="font-bold text-green-500 text-2xl md:text-4xl tracking-tight">Build a Button</h1>
+
+        <p className="text-sm md:hidden text-gray-400 text-center py-2">Tap and hold to preview animations</p>
+
+        <p className="text-sm hidden md:block text-gray-400 text-center">Hover to preview animations</p>
+
+        <div className="flex flex-col md:flex-row justify-center items-center gap-6 md:gap-12">
+          <button className="bg-green-500 w-20 h-10 text-white font-bold shadow-md">
+            Start
+          </button>
+
+          <button className="relative px-6 py-2 font-semibold rounded-xl overflow-hidden group bg-white ">
+
+            {/* TOP */}
+            <span className="absolute top-0 right-0 h-[2px] w-0 bg-green-500 group-hover:w-full group-active:w-full transition-all duration-300"></span>
+
+            {/* RIGHT */}
+            <span className="absolute top-0 right-0 w-[2px] h-0 bg-green-500 group-hover:h-full group-active:h-full transition-all duration-300 delay-150"></span>
+
+            {/* BOTTOM */}
+            <span className="absolute bottom-0 left-0 h-[2px] w-0 bg-green-500 group-hover:w-full group-active:w-full transition-all duration-300 delay-300"></span>
+
+            {/* LEFT */}
+            <span className="absolute bottom-0 left-0 w-[2px] h-0 bg-green-500 group-hover:h-full group-active:h-full transition-all duration-300 delay-500"></span>
+
+            {/* TEXT */}
+            <span className="relative text-green-600 group-hover:text-green-500 group-active:text-green-500 transition duration-300">
+              Start
+            </span>
+
+          </button>
+
+          <button className="relative px-6 py-2 font-semibold rounded-xl overflow-hidden bg-white border border-green-500 text-green-600 group">
+
+  {/* moving light sweep */}
+  <span className="absolute inset-0 bg-gradient-to-r from-transparent via-green-200 to-transparent translate-x-[-100%] group-hover:translate-x-[100%] group-active:translate-x-[100%] transition-transform duration-700"></span>
+
+  {/* text */}
+  <span className="relative z-10 group-hover:text-green-700 group-active:text-green-700 transition">
+    Start
+  </span>
+
+</button>
+        </div>
       </div>
 
     </div>
