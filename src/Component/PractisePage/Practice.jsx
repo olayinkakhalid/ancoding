993c21689be68e0b3e69cc742e716b50d305e54f
olayinkakhalid <adewoyekhalid237@gmail.com>
2026-04-16 10:51:10 +0100

    transiton for the button

--- src/Component/PractisePage/Practice.jsx
+++ src/Component/PractisePage/Practice.jsx
@@ -34,7 +34,7 @@ const Practice = () => {
       <div className="flex flex-col md:flex-row items-center justify-center gap-6 bg-gray-100 p-6 md:p-9">
 
         {/* QUICK DRILLS */}
-        <div className="relative overflow-hidden bg-green-500 hover:bg-green-600 transition-colors duration-300 p-10 text-white w-full md:w-64 text-center rounded-lg shadow-md">
+        <div className="relative overflow-hidden bg-green-500 hover:bg-green-600 transition-colors duration-300 p-10 text-white w-full md:w-64 text-center rounded-lg shadow-md" data-aos="fade-up" data-aos-duration="1000">
 
           <h2 className="text-xl font-bold animate-float">
             Quick Drills
@@ -47,7 +47,7 @@ const Practice = () => {
         </div>
 
         {/* CHALLENGES */}
-        <div className="relative overflow-hidden bg-green-600 hover:bg-green-700 transition-colors duration-300 p-14 text-white w-full md:w-96 text-center rounded-lg shadow-lg">
+        <div className="relative overflow-hidden bg-green-600 hover:bg-green-700 transition-colors duration-300 p-14 text-white w-full md:w-96 text-center rounded-lg shadow-lg" data-aos="fade-up" data-aos-duration="3000">
 
           <h2 className="text-2xl font-bold animate-float">
             Challenges
@@ -60,7 +60,7 @@ const Practice = () => {
         </div>
 
         {/* SANDBOX */}
-        <div className="relative overflow-hidden bg-green-500 hover:bg-green-700 transition-colors duration-300 p-10 text-white w-full md:w-64 text-center rounded-lg shadow-md">
+        <div className="relative overflow-hidden bg-green-500 hover:bg-green-700 transition-colors duration-300 p-10 text-white w-full md:w-64 text-center rounded-lg shadow-md" data-aos="fade-up" data-aos-duration="1000">
 
           <h2 className="text-xl font-bold animate-float">
             Sandbox
@@ -75,19 +75,19 @@ const Practice = () => {
       </div>
 
 
-      <div className="text-center space-y-2 md:space-y-3 py-4 md:py-6">
+      <div className="text-center space-y-2 md:space-y-3 py-4 md:py-6" data-aos="fade-up" data-aos-duration="2000">
         <h1 className="font-bold text-green-500 text-2xl md:text-4xl tracking-tight">Build a Button</h1>
 
         <p className="text-sm md:hidden text-gray-400 text-center py-2">Tap and hold to preview animations</p>
 
         <p className="text-sm hidden md:block text-gray-400 text-center">Hover to preview animations</p>
 
-        <div className="flex flex-col md:flex-row justify-center items-center gap-6 md:gap-12">
-          <button className="bg-green-500 w-20 h-10 text-white font-bold shadow-md">
+        <div className="flex flex-col md:flex-row justify-center items-center gap-6 md:gap-12" data-aos="fade-up" data-aos-duration="1000">
+          <button className="bg-green-500 w-20 h-10 text-white font-bold shadow-md" data-aos="fade-up" data-aos-duration="2000">
             Start
           </button>
 
-          <button className="relative px-6 py-2 font-semibold rounded-xl overflow-hidden group bg-white ">
+          <button className="relative px-6 py-2 font-semibold rounded-xl overflow-hidden group bg-white" data-aos="fade-up" data-aos-duration="3000">
 
             {/* TOP */}
             <span className="absolute top-0 right-0 h-[2px] w-0 bg-green-500 group-hover:w-full group-active:w-full transition-all duration-300"></span>
@@ -108,7 +108,7 @@ const Practice = () => {
 
           </button>
 
-          <button className="relative px-6 py-2 font-semibold rounded-xl overflow-hidden bg-white border border-green-500 text-green-600 group">
+          <button className="relative px-6 py-2 font-semibold rounded-xl overflow-hidden bg-white border border-green-500 text-green-600 group" data-aos="fade-up" data-aos-duration="4000">
 
   {/* moving light sweep */}
   <span className="absolute inset-0 bg-gradient-to-r from-transparent via-green-200 to-transparent translate-x-[-100%] group-hover:translate-x-[100%] group-active:translate-x-[100%] transition-transform duration-700"></span>
